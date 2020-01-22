8aad6f48d84a3844220a3ae2136ed59d45acacc2
Michael Taylor <mitaylor@adobe.com>
2020-01-22 16:31:43 +1300

    fix: reduce code size of `contains` function

--- src/dom-utils/contains.js
+++ src/dom-utils/contains.js
@@ -1,8 +1,10 @@
 // @flow
-
-export default function contains(parent: Element, child: Element): boolean {
-  return (
-    parent.contains(child) ||
-    (parent.shadowRoot ? parent.shadowRoot.contains(child) : false)
-  );
+export default function contains(parent: Element, child: Element) {
+  let next = child;
+  do {
+    if (next && next.isSameNode(parent)) return true;
+    // $FlowFixMe: need a better way to handle this...
+    next = next.parentNode || next.host;
+  } while (next);
+  return false;
 }
